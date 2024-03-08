51d8e66ce93094c6a85cca9bcfc5eae13fc9c6e0
Isaac Davi <izaacddavid98@gmail.com>
2024-03-08 11:03:20 -0300

    Exercício 82 e criação de pasta da aula 17

---
+++ Mundo 3/Aula 17/Exercicio83.py
@@ -0,0 +1,15 @@
+expressao = str(input('Digite a expressão: '))
+lista = []
+for simbolo in expressao:
+    if simbolo == '(':
+        lista.append('(')
+    elif simbolo == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está inválida')
