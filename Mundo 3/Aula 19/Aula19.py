fa1c3282e4284a10409a2a73de56c6bb9ed30625
Isaac Davi <izaacddavid98@gmail.com>
2024-05-03 11:45:43 -0300

    Aula 19 Teoria

---
+++ Mundo 3/Aula 19/Aula19.py
@@ -0,0 +1,48 @@
+# Title: Dicionários
+
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+pessoas['peso'] = 98.5
+'''pessoas['nome'] = 'Pedro' >> altera nome pra Pedro'''
+
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
+
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+
+for k in pessoas.values():
+    print(k)
+    
+for k,v in pessoas.items():
+    print(f'{k} = {v}')
+
+'''---------------------------------'''
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(estado1)
+print(brasil[0]['uf'])
+print(brasil[1]['sigla'])
+
+'''---------------------------------'''
+
+estado = dict()
+brasil = list()
+
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+    
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}.')
+        
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ')
+    print()
