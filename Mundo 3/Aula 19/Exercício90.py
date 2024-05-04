47fb2c20324081c271a957bfe2246ac015911019
Isaac Davi <izaacddavid98@gmail.com>
2024-05-04 11:03:15 -0300

    Correção media para aluno[media]

--- Mundo 3/Aula 19/Exercício90.py
+++ Mundo 3/Aula 19/Exercício90.py
@@ -1,17 +1,15 @@
 aluno = dict()
 aluno['nome'] = str(input('Digite o nome do aluno: '))
 aluno['media'] = float(input(f'Digite a média do {aluno["nome"]}: '))
-
-if media >= 6.0:
+if aluno['media'] >= 6.0:
     aluno['situacao'] = 'Aprovado'
 else:
     aluno['situacao'] = 'Reprovado'
 
-ficha = {'nome': nome, 'média': media, 'situação': situacao}
 for k, v in ficha.items():
     print('--------------------------------')
     print(f'{k} é igual a {v}.')
 print()
 resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
 if resp == 'N':
-    break
+    print('Fim!')
